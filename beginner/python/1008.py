3c84f4759a773cee387e7fa3a65f4485d25ca303
PedroHIegler <pedroiegler1601@gmail.com>
2023-06-29 11:10:41 -0500

    exercise-1008.py

---
+++ beginner/python/1008.py
@@ -0,0 +1,8 @@
+numFunc = int(input())
+horasTrab = int(input())
+valorGanho = float(input())
+
+ganhoTotal = horasTrab * valorGanho
+
+print("NUMBER =", numFunc)
+print("SALARY = U$ {:.2f}".format(ganhoTotal))
