93967e318d83afad232a4f63065077284c8b93cd
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-01 17:44:47 -0500

    exercise-1002.c

--- main/c/main.c
+++ main/c/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
+    double raio;
+
+    scanf("%lf", &raio);
+
+    double area = 3.14159 * pow(raio,2);
+
+    printf("A=%.4lf\n", area);
 
     return 0;
 }
