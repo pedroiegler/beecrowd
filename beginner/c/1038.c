6c9b6d51f955105641a45980ef0c38b5f5f3792a
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-07 11:57:15 -0300

    exercise 1038 | C

---
+++ beginner/c/1038.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int cod, qtd;
+
+    scanf("%d %d", &cod, &qtd);
+
+    if(cod == 1)
+        printf("Total: R$ %.2lf\n", qtd * 4.00);
+    else if(cod == 2)
+        printf("Total: R$ %.2lf\n", qtd * 4.50);
+    else if(cod == 3)
+        printf("Total: R$ %.2lf\n", qtd * 5.00);
+    else if(cod == 4)
+        printf("Total: R$ %.2lf\n", qtd * 2.00);
+    else if(cod == 5)
+        printf("Total: R$ %.2lf\n", qtd * 1.50);
+
+    return 0;
+}
