613233950fd0430edc72a337c540b642da53b165
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-10 20:57:48 -0300

    exercise 1047 | Java

--- main/java/project/src/Main.java
+++ main/java/project/src/Main.java
@@ -9,6 +9,21 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+		int horaIni, horaFim, minIni, minFim, dif;
+        horaIni = scanner.nextInt();
+        minIni = scanner.nextInt();
+        horaFim = scanner.nextInt();
+        minFim = scanner.nextInt();
+
+        dif = ((horaFim * 60) + minFim) - ((horaIni * 60) + minIni);
+
+        if (dif <= 0) {
+            dif += 24 * 60;
+        }
+
+        System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)%n", dif / 60, dif % 60);
+	
         scanner.close();
     }
 }
+
