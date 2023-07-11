3f82e538e4fea1e309b7f2e7e31aaf085087c36a
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-10 21:09:23 -0300

    exercise 1052 | C

--- main/c/main.c
+++ main/c/main.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    int horaIni, horaFim, minIni, minFim, dif;
-    scanf("%d %d %d %d", &horaIni, &minIni, &horaFim, &minFim);
-
-    dif = ((horaFim * 60) + minFim) - ((horaIni * 60) + minIni);
-
-    if(dif <= 0) 
-        dif += 24 * 60;
-
-    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", dif/60, dif%60);
 
     return 0;
     
