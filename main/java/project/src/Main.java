c0cf109707ed34787bf9c7aaf51c3c84aa9c3a71
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-12 09:25:34 -0300

    exercise 1065 | Java

--- main/java/project/src/Main.java
+++ main/java/project/src/Main.java
@@ -9,16 +9,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        int i, n, cont = 0;
-
-        for(i = 0; i < 5; i++){
-            n = scanner.nextInt();
-            if(n % 2 == 0){
-                cont+=1;
-            }
-        }
-
-        System.out.println(cont + " valores pares");
+        scanner.close();
 
     }
 }
