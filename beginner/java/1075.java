252ef7df82ec169e3c762e50bd2382b3343cd39a
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-15 16:18:35 -0300

    exercise 1075 | Java

--- beginner/java/1075.java
+++ beginner/java/1075.java
@@ -9,6 +9,16 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int n, i;
+
+        n = scanner.nextInt();
+
+        for(i = 0; i < 10000; i++){
+            if(i % n == 2){
+                System.out.println(i);
+            }
+        }
+
         scanner.close();
 
     }
