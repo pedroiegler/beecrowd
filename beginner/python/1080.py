024d6127df3d433a61ec30cf5a3e70f98a76c7a3
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-16 19:52:58 -0300

    exercise 1080 | Python

---
+++ beginner/python/1080.py
@@ -0,0 +1,14 @@
+i = 1 
+maior = 0
+posicao = 0
+
+for i in range(1,101):
+    n = int(input())
+
+    if(n > maior):
+        maior = n
+        posicao = i
+        
+
+print(maior)
+print(posicao)
