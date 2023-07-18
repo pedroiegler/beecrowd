300c3cea8af4c282deb391e44175b1610e995b70
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-18 08:31:06 -0300

    exercise 1097 | C

--- main/c/main.c
+++ main/c/main.c
@@ -1,39 +1,6 @@
 #include <stdio.h>
-int main(){
 
-    int i, n, quant, contR = 0, contS = 0, contC = 0, contT = 0;
-    double mediaS = 0, mediaR = 0, mediaC = 0;
-    char tipo;
-
-    scanf("%d", &n);
-
-    for(i = 0; i < n; i++){
-        scanf("%d %c", &quant, &tipo);
-
-        contT += quant;
-
-        if(tipo == 'R')
-            contR += quant;
-        else if(tipo == 'S')
-            contS += quant;
-        if(tipo == 'C')
-            contC += quant;
-        
-    }
-
-    mediaS = (contS * 100.0) / contT;
-    mediaR = (contR * 100.0) / contT;
-    mediaC = (contC * 100.0) / contT;
-
-    printf("Total: %d cobaias\n", contT);
-    printf("Total de coelhos: %d\n", contC);
-    printf("Total de ratos: %d\n", contR);
-    printf("Total de sapos: %d\n", contS);
-    printf("Percentual de coelhos: %.2lf %%\n", mediaC);
-    printf("Percentual de ratos: %.2lf %%\n", mediaR);
-    printf("Percentual de sapos: %.2lf %%\n", mediaS);
-
+int main() {
 
     return 0;
-    
 }
--- main/java/project/src/Main.java
+++ main/java/project/src/Main.java
@@ -9,17 +9,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        int i, j;
-
-        for (i = 1, j = 7; i <= 9; j--) {
-            System.out.printf("I=%d J=%d%n", i, j);
-
-            if (j == 5) {
-                i += 2;
-                j = 8;
-            }
-        }
-
         scanner.close();
 
     }
