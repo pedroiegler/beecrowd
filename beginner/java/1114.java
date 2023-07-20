9fc85bb70cdc01a0f0e6d1cd56b779d8297648b2
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-19 23:12:27 -0300

    exercise 1114 | Java

---
+++ beginner/java/1114.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int senha;
+
+        while(true){
+            senha = scanner.nextInt();
+
+            if(senha == 2002){
+                System.out.printf("Acesso Permitido%n");
+                break;
+            } else
+                System.out.printf("Senha Invalida%n");
+        }
+
+        scanner.close();
+
+    }
+}
+
