76ed831c4a93c86da6ce36bf1a46133f61eb868f
PedroHIegler <pedroiegler1601@gmail.com>
2023-07-21 08:55:59 -0300

    exercise 1118 | C

--- beginner/c/1118.c
+++ beginner/c/1118.c
@@ -2,26 +2,26 @@
 
 int main() {
     
-    float n1, n2, i, media = 0;
+    float n1, n2, media = 0;
     int nota_valida = 0, x = 1;
 
-    while(x == 1){
-        while(nota_valida != 1){
+    do {
+        while (nota_valida != 1) {
             scanf("%f", &n1);
-            if(n1 < 0.0 || n1 > 10.0)
+            if (n1 < 0.0 || n1 > 10.0)
                 printf("nota invalida\n");
             else
-                nota_valida+=1;
+                nota_valida = 1;
         }
 
         nota_valida = 0;
 
-        while(nota_valida != 1){
+        while (nota_valida != 1) {
             scanf("%f", &n2);
-            if(n2 < 0.0 || n2 > 10.0)
+            if (n2 < 0.0 || n2 > 10.0)
                 printf("nota invalida\n");
             else
-                nota_valida+=1;
+                nota_valida = 1;
         }
 
         nota_valida = 0;
@@ -30,17 +30,12 @@ int main() {
 
         printf("media = %.2f\n", media);
 
-        printf("novo calculo (1-sim 2-nao)\n");
-        scanf("%d", &x);
-
-        if(x != 1 && x != 2){
+        do {
             printf("novo calculo (1-sim 2-nao)\n");
             scanf("%d", &x);
-        } else if(x == 2){
-            break;
-        }
+        } while (x != 1 && x != 2);
 
-    }
+    } while (x == 1);
     
     return 0;
 }
