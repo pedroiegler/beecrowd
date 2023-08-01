7c8529f991951b69a8d2cdbae9c7126c4baf59fe
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-01 15:33:54 -0300

    exercise 1847 | Python

---
+++ beginner/python/1847.py
@@ -0,0 +1,26 @@
+a, b, c = list(map(int, input().split()))
+
+if(a > b):
+    if(b > c):
+        if((b-c)<(a-b)):
+           print(":)")
+        else:
+           print(":(")
+    
+    else:
+       print(":)")
+
+elif(b > a):
+    if(c > b):
+        if((c-b)<(b-a)): 
+           print(":(")
+        else:
+           print(":)")
+    
+    else:
+       print(":(")
+
+elif(c > b): 
+   print(":)")
+else:
+   print(":(")
