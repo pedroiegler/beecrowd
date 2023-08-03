309f09279a61f397066e7acec5f9ad8565c6749a
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-03 08:41:42 -0300

    exercise 1184 | C

--- beginner/c/1184.c
+++ beginner/c/1184.c
@@ -8,27 +8,25 @@ int main() {
 
     scanf(" %c", &operacao);
 
-    for(i = 0; i < 12; i++){
-        for(j = 0; j < 12; j++){
+    for (i = 0; i < 12; i++) {
+        for (j = 0; j < 12; j++) {
             scanf("%lf", &m[i][j]);
         }
     }
 
-    int counter = 1;
     int cont = 0;
-    while(counter < 12){
-        for (i = 1; i <= counter; i++) {
-            for (j = 1; j <= counter; j++) { 
+    for (i = 0; i < 12; i++) {
+        for (j = 0; j < 12; j++) {
+            if (i > j) {
                 soma += m[i][j];
                 cont++;
             }
-            counter++;
         }
     }
 
-    if(operacao == 'S'){
+    if (operacao == 'S') {
         printf("%.1lf\n", soma);
-    } else if(operacao == 'M'){
+    } else if (operacao == 'M') {
         printf("%.1lf\n", soma / cont);
     }
 
