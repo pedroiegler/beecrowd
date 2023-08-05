e6e4c8c602d88b06466be356c36aff547ab05321
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-05 17:09:44 -0300

    exercise 1187 | Python

---
+++ beginner/python/1187.py
@@ -0,0 +1,28 @@
+operacao = input()
+
+m = [[0 for j in range(12)] for i in range(12)]
+
+for i in range(12):
+    for j in range(12):
+        m[i][j] = float(input())
+
+def soma_or_media(mat, op):
+    coluna = 11
+    cont = 0
+    soma = 0.0
+    aux = 1
+
+    for i in range(5):
+        for j in range(aux, coluna):
+            soma += mat[i][j]
+            cont += 1
+            
+        coluna-=1
+        aux+=1
+
+    if op == 'S':
+        return soma
+    else:
+        return soma/cont
+
+print("%.1f" % (soma_or_media(m, operacao)))
