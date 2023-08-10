82454e6f1724e6bdd2ed10ee280c9dc9122bb016
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-10 08:26:19 -0300

    exercise 1557 | Java

--- main/java/project/src/Main.java
+++ main/java/project/src/Main.java
@@ -6,24 +6,52 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-    	int n;
+    	int n, i, j;
+		int elemento;
 
-        while (scanner.hasNext()) {
-    		n = scanner.nextInt();
-            
-    		for (int i = 0; i < n; i++) {
-    			for (int j = 0; j < n; j++) {
-    				if (j == n - i - 1) 
-                        System.out.print(2);
+		while (true){
 
-    				else if (i == j) 
-                        System.out.print(1);
+			n = scanner.nextInt();
 
-    				else 
-                        System.out.print(3);
-    			}
-    			System.out.print("\n");
-    		}
-    	}
+			if (n == 0)
+				break;
+
+			int[][] m = new int[n][n];
+
+			elemento = 1;
+
+			for (i = 0; i < n; i++){
+				for (j = 0; j < n; j++){
+					m[i][j] = elemento;
+					elemento *= 2;
+				}
+				elemento = (m[i][0]) * 2;
+			}
+
+			int digitos = 0;
+			int t;
+
+			t = (m[n - 1][n - 1]);
+
+			do{
+
+				t %= 10;
+				t = t / 10;
+				digitos++;
+
+			} while (t > 0);
+
+			for (i = 0; i < n; i++) {
+                for (j = 0; j < n; j++) {
+                    if (j == 0)
+                        System.out.printf("%" + digitos + "d", m[i][j]);
+                    else
+                        System.out.printf(" %" + digitos + "d", m[i][j]);
+                }
+                System.out.println();
+            }
+
+            System.out.println();
+		}
     }
 }
