a006de0c7cff94d137b15d3cc8693a33db727e9b
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-10 08:17:08 -0300

    exercise 1759 | C

--- beginner/c/1759.c
+++ beginner/c/1759.c
@@ -2,15 +2,53 @@
 
 int main (){
 
-  int n, i;
+	unsigned short int n, i, j;
+ 	int elemento;
 
-  scanf("%d", &n);
+	while (1){
 
-  for(i = 1; i <= n; i++){
-    if(i == n)
-      printf("Ho!\n");
-    else
-      printf("Ho ");
-  }
+		scanf("%hd", &n);
 
+		if (n == 0)
+			return 0;
+
+		int m[n][n];
+
+		elemento = 1;
+
+		for (i = 0; i < n; i++){
+			for (j = 0; j < n; j++){
+				m[i][j] = elemento;
+				elemento *= 2;
+			}
+			elemento = (m[i][0]) * 2;
+		}
+
+		unsigned short int digitos = 0;
+		int t;
+
+		t = (m[n - 1][n - 1]);
+
+		do{
+
+			t % 10;
+			t = t/10;
+			digitos++;
+
+		} while (t > 0);
+
+		for (i = 0; i < n; i++){
+			for (j = 0; j < n; j++){
+				if (j == 0)
+					printf("%*d", digitos, m[i][j]);
+				else
+					printf(" %*d", digitos, m[i][j]);
+			}
+
+			printf("\n");
+		}
+
+		printf("\n");
+
+	}
 }
--- main/c/main.c
+++ main/c/main.c
@@ -3,27 +3,52 @@
 int main (){
 
 	unsigned short int n, i, j;
+ 	int elemento;
 
-	while (scanf("%hd", &n) != EOF){
-		short m[n][n];
+	while (1){
 
-		for (i = 0; i < n; i++)
-			for (j = 0; j < n; j++)
-			{
-				if (i == j)
-					m[i][j] = 1;
-				if (i == n - j - 1)
-					m[i][j] = 2;
-				if (i != j && i != n - j - 1)
-					m[i][j] = 3;
+		scanf("%hd", &n);
+
+		if (n == 0)
+			return 0;
+
+		int m[n][n];
+
+		elemento = 1;
+
+		for (i = 0; i < n; i++){
+			for (j = 0; j < n; j++){
+				m[i][j] = elemento;
+				elemento *= 2;
 			}
+			elemento = (m[i][0]) * 2;
+		}
 
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-				printf("%d", m[i][j]);
+		unsigned short int digitos = 0;
+		int t;
+
+		t = (m[n - 1][n - 1]);
+
+		do{
+
+			t % 10;
+			t = t/10;
+			digitos++;
+
+		} while (t > 0);
+
+		for (i = 0; i < n; i++){
+			for (j = 0; j < n; j++){
+				if (j == 0)
+					printf("%*d", digitos, m[i][j]);
+				else
+					printf(" %*d", digitos, m[i][j]);
+			}
 
 			printf("\n");
 		}
+
+		printf("\n");
+
 	}
 }
