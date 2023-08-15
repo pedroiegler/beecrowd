02cdc864074c8b3427ad07c893228929a63d39a5
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-15 08:23:04 -0300

    exercise 1589 | C

---
+++ beginner/c/1589.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+
+	int a, b, i, n;
+
+	scanf("%d", &n);
+
+	for(i = 0; i < n; i++){
+		scanf("%d %d", &a, &b);
+
+		printf("%d\n", a+b);
+	}
+}
