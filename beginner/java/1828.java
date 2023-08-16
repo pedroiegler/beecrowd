37c4fc378aca140f03f7cd6669641194a6d32d70
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-16 08:43:16 -0300

    exercise 1828 | Java

---
+++ beginner/java/1828.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        int t = entrada.nextInt();
+        entrada.nextLine();
+        for(int i=1;i<=t;i++){
+            String partida = entrada.nextLine();
+            String mao[] = partida.split(" ");
+            if(mao[0].equals(""+mao[1])){
+                System.out.println("Caso #"+i+": De novo!");
+            }else{
+                if(mao[0].equals("tesoura") && (mao[1].equals("papel") || mao[1].equals("lagarto"))){
+                    System.out.println("Caso #"+i+": Bazinga!");
+                }else if(mao[0].equals("papel") && (mao[1].equals("pedra") || mao[1].equals("Spock"))){
+                    System.out.println("Caso #"+i+": Bazinga!");
+                }else if(mao[0].equals("pedra") && (mao[1].equals("lagarto") || mao[1].equals("tesoura"))){
+                    System.out.println("Caso #"+i+": Bazinga!");
+                }else if(mao[0].equals("lagarto") && (mao[1].equals("papel") || mao[1].equals("Spock"))){
+                    System.out.println("Caso #"+i+": Bazinga!");
+                }else if(mao[0].equals("Spock") && (mao[1].equals("tesoura") || mao[1].equals("pedra"))){
+                    System.out.println("Caso #"+i+": Bazinga!");
+                }else{
+                    System.out.println("Caso #"+i+": Raj trapaceou!");
+                }
+            }
+        }
+    }
+}
