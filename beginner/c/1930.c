ef25137ad1656348c10b43cd827efac5b2e290dd
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-21 14:23:43 -0300

    exercise 1930 | C

---
+++ beginner/c/1930.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n1, n2, n3, n4;
+    
+    scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
+    
+    printf("%d\n", ((n1 + n2 + n3 + n4) - 3));
+ 
+    return 0;
+}
