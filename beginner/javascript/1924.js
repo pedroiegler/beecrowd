08772ef12b56bb8c9c83acc63dd8b4a37af26c0d
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-23 22:21:52 -0300

    exercise 1924 | JavaScript

---
+++ beginner/javascript/1924.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+var linhas = input.split('\n');
+
+m = parseInt(linhas.shift());
+
+for(var i = 0; i < m; i++) 
+    ch = linhas.shift();
+
+console.log("Ciencia da Computacao");
+
