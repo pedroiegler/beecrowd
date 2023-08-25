3bc9bacb89ec0d6551025b3c5f09d6ccf017acc4
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-25 11:42:31 -0300

    exercise 1933 | C

---
+++ beginner/c/1933.c
@@ -0,0 +1,14 @@
+int main() {
+    
+    int carta1, carta2;
+    
+    scanf("%d %d", &carta1, &carta2);
+    
+    if (carta2 > carta1)
+        printf("%d\n", carta2);
+        
+    else 
+        printf("%d\n", carta1);
+
+    return 0;
+}
