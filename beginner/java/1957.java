f1895922a9bece4f8ccfce897766a30d2cf0cc6d
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-27 12:37:45 -0300

    exercise 1957 | Java

---
+++ beginner/java/1957.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+        System.out.printf("%X%n", numero);
+    }
+}
