573c48c985ab9ee3c69cac33cd4ea236e1105e6e
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-28 08:57:14 -0300

    exercise 3473 | Java

---
+++ ad-hoc/java/3473.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+ 
+        int n;
+    
+        n = entrada.nextInt();
+        
+        System.out.printf("%d%n", n+1);
+    }
+}
