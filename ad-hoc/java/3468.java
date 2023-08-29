78cad22ad976f3a02979ab6eecd59a97dfee30f1
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-29 08:20:17 -0300

    exercise 3468 | Java

---
+++ ad-hoc/java/3468.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+ 
+        String idea;
+        idea = entrada.nextLine().toLowerCase();
+
+        if (idea.equals("oposicao") || idea.equals("contrariedade")) {
+            System.out.println("mas");
+        } else if (idea.equals("quantidade") || idea.equals("intensidade")) {
+            System.out.println("mais");
+        }
+    }
+}
+public class 3468 {
+    
+}
