188466378fbaeb760e2f85fef36479f109b2b98d
PedroHIegler <pedroiegler1601@gmail.com>
2023-09-03 00:13:18 -0300

    exercise 1983 | Java

--- beginner/java/1983.java
+++ beginner/java/1983.java
@@ -1,3 +1,32 @@
-public class 1983 {
-    
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+	
+    public static void main(String[] args) throws IOException {
+    	Scanner scanner = new Scanner(System.in);
+
+    	int n, i, matricula, matricular_maior = 0;
+		double nota, maior = 0;
+
+		n = scanner.nextInt();
+
+		for(i = 0; i < n; i++){
+			matricula = scanner.nextInt();
+			nota = scanner.nextDouble();
+
+			if(nota > maior){
+				maior = nota;
+				matricular_maior = matricula;
+			}
+
+		}
+
+		if(maior >= 8){
+			System.out.printf("%d%n", matricular_maior);
+		} else{
+			System.out.printf("Minimum note not reached%n");
+    }
+    }
+	
 }
