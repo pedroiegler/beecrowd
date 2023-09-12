ac2b995d0bc51c72e0695f469ccf53accab97db5
PedroHIegler <pedroiegler1601@gmail.com>
2023-09-12 08:50:53 -0300

    exercise 3475 | Python

---
+++ ad-hoc/python/3475.py
@@ -0,0 +1,42 @@
+numero = input()
+
+if numero == "zero":
+    print("0")
+elif numero == "um":
+    print("1")
+elif numero == "dois":
+    print("2")
+elif numero == "tres":
+    print("3")
+elif numero == "quatro":
+    print("4")
+elif numero == "cinco":
+    print("5")
+elif numero == "seis":
+    print("6")
+elif numero == "sete":
+    print("7")
+elif numero == "oito":
+    print("8")
+elif numero == "nove":
+    print("9")
+elif numero == "0":
+    print("zero")
+elif numero == "1":
+    print("um")
+elif numero == "2":
+    print("dois")
+elif numero == "3":
+    print("tres")
+elif numero == "4":
+    print("quatro")
+elif numero == "5":
+    print("cinco")
+elif numero == "6":
+    print("seis")
+elif numero == "7":
+    print("sete")
+elif numero == "8":
+    print("oito")
+elif numero == "9":
+    print("nove")
