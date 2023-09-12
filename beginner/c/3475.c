d142643f82f4133eccf651e41c87bbd7ded6454a
PedroHIegler <pedroiegler1601@gmail.com>
2023-09-12 08:45:55 -0300

    exercise 3475 | C

---
+++ beginner/c/3475.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char numero[20];
+
+    scanf("%s", numero);
+
+    if (strcmp(numero, "zero") == 0) {
+        printf("0\n");
+    } else if (strcmp(numero, "um") == 0) {
+        printf("1\n");
+    } else if (strcmp(numero, "dois") == 0) {
+        printf("2\n");
+    } else if (strcmp(numero, "tres") == 0) {
+        printf("3\n");
+    } else if (strcmp(numero, "quatro") == 0) {
+        printf("4\n");
+    } else if (strcmp(numero, "cinco") == 0) {
+        printf("5\n");
+    } else if (strcmp(numero, "seis") == 0) {
+        printf("6\n");
+    } else if (strcmp(numero, "sete") == 0) {
+        printf("7\n");
+    } else if (strcmp(numero, "oito") == 0) {
+        printf("8\n");
+    } else if (strcmp(numero, "nove") == 0) {
+        printf("9\n");
+    } else if (strcmp(numero, "0") == 0) {
+        printf("zero\n");
+    } else if (strcmp(numero, "1") == 0) {
+        printf("um\n");
+    } else if (strcmp(numero, "2") == 0) {
+        printf("dois\n");
+    } else if (strcmp(numero, "3") == 0) {
+        printf("tres\n");
+    } else if (strcmp(numero, "4") == 0) {
+        printf("quatro\n");
+    } else if (strcmp(numero, "5") == 0) {
+        printf("cinco\n");
+    } else if (strcmp(numero, "6") == 0) {
+        printf("seis\n");
+    } else if (strcmp(numero, "7") == 0) {
+        printf("sete\n");
+    } else if (strcmp(numero, "8") == 0) {
+        printf("oito\n");
+    } else if (strcmp(numero, "9") == 0) {
+        printf("nove\n");
+    } 
+
+    return 0;
+}
