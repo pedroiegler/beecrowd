583b97df0a7722296b21320d5cfd4f4b92d53139
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-01 13:29:58 -0300

    exercise 2165 | C

---
+++ beginner/c/2165.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+	unsigned short qtsCaracteres, i;
+	char texto[510];
+
+	scanf("%[^\n]", texto);
+
+	i = 0;
+	while (texto[i++])
+		qtsCaracteres++;
+
+
+	if (qtsCaracteres > 140)
+		printf("MUTE\n");
+	else
+		printf("TWEET\n");
+
+}
