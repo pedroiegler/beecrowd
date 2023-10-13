1c1fe55beae77e2bf997eb9c8385a5ebdd60c839
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-13 11:38:50 -0300

    exercise 2510 | JavaScript

---
+++ beginner/javascript/2510.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var input = fs.readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+var n = parseInt(lines.shift());
+
+for(var i = 0; i < n; i++){
+    bandido = lines.shift();
+    console.log("Y");
+} 
