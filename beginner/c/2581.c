8398ea90581de24b1d885555ab52551fc6c114ff
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-16 08:25:00 -0300

    exercise 2581 | C

---
+++ beginner/c/2581.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    char pergunta[50];
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++){
+        scanf("%s", pergunta);
+
+        printf("I am Toorg!\n");
+    }
+
+    return 0;
+}
