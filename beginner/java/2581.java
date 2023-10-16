a8d3e31c584666b56680039965ff98b391668b46
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-16 08:28:05 -0300

    exercise 2581 | Java

---
+++ beginner/java/2581.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n, i;
+        String pergunta;
+
+        n = scanner.nextInt();
+
+        for(i = 0; i < n; i++){
+            pergunta = scanner.next();
+
+            System.out.printf("I am Toorg!%n");
+        }
+    }
+}
