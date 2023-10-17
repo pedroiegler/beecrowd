1f6eea30353d94cea20a2148c28f1fdab9ebecc1
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-17 08:09:51 -0300

    exercise 2582 | Python

---
+++ beginner/python/2582.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for i in range(n):
+    n1, n2 = list(map(int, input().split()))
+
+    soma = 0
+    soma = n1 + n2
+    
+    if (soma == 0): print("PROXYCITY")
+    elif (soma == 1): print("P.Y.N.G.")
+    elif (soma == 2): print("DNSUEY!")
+    elif (soma == 3): print("SERVERS")
+    elif (soma == 4): print("HOST!")
+    elif (soma == 5): print("CRIPTONIZE")
+    elif (soma == 6): print("OFFLINE DAY")
+    elif (soma == 7): print("SALT")
+    elif (soma == 8): print("ANSWER!")
+    elif (soma == 9): print("RAR?")
+    elif (soma == 10): print("WIFI ANTENNAS")
+    
