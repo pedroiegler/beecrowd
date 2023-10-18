ed23eec0e61c456fb529d4021f752cb2c4c7d8d2
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-18 09:09:39 -0300

    exercise 2167 | C

---
+++ beginner/c/2167.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    int i;
+    int maior;
+    int posicao = 0;
+
+    scanf("%d", &N);
+
+    int vetor[N];
+
+    for (i = 0; i< N; i++){
+        scanf("%d", &vetor[i]);
+    }
+
+    maior = vetor[0];
+
+    for(i = 0; i < N-1; i++){
+        if (vetor[i+1] > maior){
+            maior = vetor[i+1];
+        }
+        
+        if (vetor[i+1] < maior){
+            posicao = i+2;
+            break;
+        }
+    }
+
+    printf("%d\n", posicao);
+
+    return 0;
+}
