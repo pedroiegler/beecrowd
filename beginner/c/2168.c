07abc34f1cd2006a95bc208f90bd804a96420e6c
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-18 09:50:12 -0300

    exercise 2168 | C

---
+++ beginner/c/2168.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int cameras[N+1][N+1];
+    char quadras[N][N];
+
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= N; j++) {
+            scanf("%d", &cameras[i][j]);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int camerasAtivadas = cameras[i][j] + cameras[i][j+1] + cameras[i+1][j] + cameras[i+1][j+1];
+            if (camerasAtivadas >= 2) {
+                quadras[i][j] = 'S';
+            } else {
+                quadras[i][j] = 'U';
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%c", quadras[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
