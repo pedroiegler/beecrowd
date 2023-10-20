03a585456c1d1e029f0fae445479f915c090660c
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-20 10:37:48 -0300

    exercise 2534 | JavaScript

---
+++ beginner/javascript/2534.js
@@ -0,0 +1,41 @@
+function bubbleSort(arr) {
+    const n = arr.length;
+    let temp;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question((input) => {
+    const [n, q] = input.split(' ').map(Number);
+    rl.question((numbersInput) => {
+        const numbers = numbersInput.split(' ').map(Number);
+
+        bubbleSort(numbers);
+
+        for (let i = 0; i < q; i++) {
+            rl.question((positionInput) => {
+                const position = parseInt(positionInput);
+
+                if (position >= 1 && position <= n) 
+                    console.log(numbers[position - 1]);
+
+                if (i === q - 1) 
+                    rl.close();
+            });
+        }
+    });
+});
