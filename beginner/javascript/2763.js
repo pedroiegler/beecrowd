5a65053841202d54b48e9d748b5e13f34fb5498f
PedroHIegler <pedroiegler1601@gmail.com>
2023-10-25 09:24:52 -0300

    exercise 2763 | JavaScript

--- beginner/javascript/2763.js
+++ beginner/javascript/2763.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('stdin', 'utf8');
 const lines = input.trim().split('\n');
-
+ 
 const tokens = lines.shift().split(/[.-]/);
 
   tokens.forEach((token) => { 
